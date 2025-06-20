63dc92e76b8aef7067d7dd148fe6ee319e363644
Doni Maulana <146922643+donimaulanaa@users.noreply.github.com>
2025-06-20 15:53:19 +0700

    Add files via upload

---
+++ 17_1.cpp
@@ -0,0 +1,15 @@
+#include <conio.h>
+#include <stdio.h>
+#include <iostream>
+
+garis(){
+	printf("\n----------------\n");
+}
+
+using namespace std;
+
+int main(){
+	garis();
+	cout << "AMIK BSI - Pondok Labu" << endl;
+	garis();
+}
